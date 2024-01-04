1ecb35fbdd95d6828fa3be835ba4791f4d58925d
Daniel Turner <lunartechfreek@googlemail.com>
2024-01-04 19:03:40 +0000

    Add function to return random word

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
+import random
+import words 
+
+def get_word(difficulty_selected):
+    """
+    Function to return random word, taking
+    a parameter of game difficulty chosen
+    """
+    word = random.choice(difficulty_selected)
+    return word.upper()
 
