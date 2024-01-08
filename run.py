a9a28cc6845512c28b17bd09c24e8fd1e2a75c1b
Daniel Turner <lunartechfreek@googlemail.com>
2024-01-08 17:40:31 +0000

    Add welcome function

--- run.py
+++ run.py
@@ -17,3 +17,26 @@ def run_story(story):
     """
     print(story)
 
+def welcome():
+    """
+    Function to handle the start of the game
+    """
+    run_story(story.intro)
+
+    while True:
+        # Loop to ask user if they would like to play
+        user_input = input('Do you accept the challange? (Y/N)').upper()  
+
+        if user_input == 'N':
+            print('Goodbye!')
+            break
+        elif user_input == 'Y':
+            print('Lets Play!...\n')
+        else:
+            # Handles invalid input by the user
+            print('Invalid input. Please enter Y or N.\n')
+
+def main():
+    welcome()
+
+main()
