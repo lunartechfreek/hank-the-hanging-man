71317d51400377e7ec82fcd14735a5e192df2dbd
Daniel Turner <lunartechfreek@googlemail.com>
2024-01-08 16:54:27 +0000

    Import story file and add run_story function

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import random
 import words 
+import story
 
 def get_word(difficulty_selected):
     """
@@ -8,4 +9,11 @@ def get_word(difficulty_selected):
     """
     word = random.choice(difficulty_selected)
     return word.upper()
+
+def run_story(story):
+    """
+    Function to display relevant section
+    of story narrative
+    """
+    print(story)
 
