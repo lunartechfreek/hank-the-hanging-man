9efad56f1686dd0073c6d889f8d0e5855cc9294c
Daniel Turner <lunartechfreek@googlemail.com>
2024-01-09 18:40:32 +0000

    Add narratives for difficulty chosen

--- story.py
+++ story.py
@@ -1,7 +1,7 @@
-# File to store all story narrative
+# File to store all story narratives
 
 # Intoduction narrative
-intro = '''
+intro = """
 Welcome to Hank the Hanging Man!\n
 The year is 1705 and you were just in the middle of taking your annual bath,
 when suddenly you get a knock at the door because your
@@ -20,4 +20,29 @@ Or shall you let Hank hang for his stupidity?\n
 Type Y to accept and save Hank!
 Type N to admit you are also an idiot, and let Hank hang!
 P.S. Admitting you are also an idiot, may result in your own hanging!
-'''
+"""
+
+# Narrative for easy difficulty
+easy = """
+Oh so you chose easy eh? 🤨\n
+Hank thought that you were the smart one, but now he's not sure...
+he starts sweating as they put the noose around his neck!
+"""
+
+# Narrative for medium difficulty
+medium = """
+Ok, so you have chosen medium 🙂\n
+Hank is somewhat filled with confidence, but has to consider that
+you are neither confirming or denying your intelligence…
+He hopes you’re not this indecisive while playing!
+His life depends on it!
+"""
+
+# Narrative for hard difficulty
+hard = """
+Well look at this, you have chosen hard 🤓
+Hank is filled with the upmost confidence in you!
+But has to consider that you have just made the game that 
+decides his fate even harder for yourself!
+Let’s hope this bravery pays off…Hanks life depends on it!
+"""
