18139dfa1bf619f0c26ce96aef59b14561108e32
Daniel Turner <lunartechfreek@googlemail.com>
2024-01-13 13:05:20 +0000

    Update intro to fit on deployed box

--- story.py
+++ story.py
@@ -4,19 +4,15 @@
 intro = """
 Welcome to Hank the Hanging Man!\n
 The year is 1705 and you were just in the middle of taking your annual bath,
-when suddenly you get a knock at the door because your
-good buddy Hank has got himself into a bit of a pickle! 
-The villagers say that Hank is a bit of an idiot, 
-and is bringing down peoples perception of the community!
-So they have taken it upon themselves to hang him in the town square!\n
-The only way they will let Hank survive is if you agree to take him
-in and teach him how to function As a human being!
-But in order to do that the villagers need to make sure
-that you are not an idiot as well!
-So they challenge you play a game of hangman... (ironic eh?)
-to prove your intelligence!\n
-Do you accept the villagers challenge?
-Or shall you let Hank hang for his stupidity?\n
+when suddenly you get a knock at the door because your good buddy Hank has 
+got himself into a bit of a pickle! The villagers say that Hank is a bit of 
+an idiot, and is bringing down peoples perception of the community!
+So they have taken it upon themselves to hang him in the town square!
+The only way they will let Hank live is if you agree to take him in and teach
+him how to function As a human being! But in order to do that the villagers 
+need to make sure that you are not an idiot as well! So they challenge
+you to a game of hangman... (ironic eh?), to prove your intelligence!\n
+Do you accept the challenge? Or shall you let Hank hang for his stupidity?
 Type Y to accept and save Hank!
 Type N to admit you are also an idiot, and let Hank hang!
 P.S. Admitting you are also an idiot, may result in your own hanging!
