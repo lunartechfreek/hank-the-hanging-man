98b567c38737625b0f68cabb888d4e6ab640182c
Daniel Turner <lunartechfreek@googlemail.com>
2024-01-18 20:23:24 +0000

    Add letters guessed to only display if tries > 0

--- run.py
+++ run.py
@@ -266,7 +266,7 @@ def play(word, user_name):
         # Displays the length of word if the user has tries left
         if TRIES > 0:
             print(f'\nThe word is {len(word)} letters long')
-        print(f'Letters guessed: {", ".join(guessed_letters)}\n')
+            print(f'\nLetters guessed: {", ".join(guessed_letters)}\n')
 
     game_end(word, user_name)
 
